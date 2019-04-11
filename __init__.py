501f8c68c8aebc03e2a0d2696e0e80b1027ff44d
chrissyblissy <39778083+chrissyblissy@users.noreply.github.com>
2019-04-11 17:06:41 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,338 @@
+import os
+
+from flask import Flask, render_template, request, redirect, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+from tempfile import mkdtemp
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.sql import table, column, select, update, insert
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mysql import insert
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# configure application and database
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://updatepantry:K269210dL1ghtn1ng@localhost/thepantry'
+db = SQLAlchemy(app)
+engine = create_engine('mysql://updatepantry:K269210dL1ghtn1ng@localhost/thepantry')
+connection = engine.connect()
+metadata = MetaData(bind=engine)
+sqlsession =  sessionmaker(bind=engine)()
+
+### Helper functions ###
+
+# inserts a value into an inputted column if it does not already exist
+def insertifnot(columnvalue, value):
+    itemexists = False
+    for item in db.engine.execute("SELECT * FROM " + columnvalue + "s"):
+        if value.lower() == item[1].lower():
+            itemexists = True
+            return item[0]
+    if itemexists == False:
+        sqlsession.execute(insert(Table(columnvalue + 's', metadata, autoload=True)).values({columnvalue +"_name": value}))
+        lastid = sqlsession.execute("SELECT LAST_INSERT_ID() AS " + columnvalue + "id")
+        for row in lastid:
+            return row[columnvalue + 'id']
+
+# creates decorators for webpages that users must be logged into to be able to see
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+###### initial pages for registering and logging in ######
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    """Register user for website."""
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method =="POST":
+
+        inputusername=request.form.get("username")
+        inputpassword=request.form.get("password")
+        # Ensure username was submitted
+        if not inputusername:
+            return apology("you must provide a username")
+
+        # Ensure password was submitted
+        elif not inputpassword:
+            return apology("you must provide a password")
+
+        # Ensure password match
+        elif inputpassword != request.form.get("confirmation"):
+            return apology("passwords do not match")
+
+        # Hashes the users password
+        hashpassword = generate_password_hash(inputpassword)
+
+        for row in db.engine.execute("SELECT user_name FROM users"):
+            # checks if username has been taken.
+            if inputusername.lower() == row[0].lower():
+                return apology("That username has been taken")
+        
+        # enter username and hashed password into the users database
+        sqlsession.execute(insert(Table('users', metadata, autoload=True)).values({
+            "user_name": inputusername,
+            "user_hash": hashpassword
+            }))
+
+        # Remember which user has logged in
+        lastid = sqlsession.execute("SELECT LAST_INSERT_ID() AS user_id")
+        for row in lastid:
+            session["user_id"] = row['user_id']
+        
+        # commit changes to the database
+        sqlsession.commit() 
+
+        #Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    #session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        inputusername=request.form.get("username")
+        inputpassword=request.form.get("password")
+        # Ensure username was submitted
+        if not inputusername:
+            return apology("you must provide a username")
+
+        # Ensure password was submitted
+        elif not inputpassword:
+            return apology("must provide password")
+
+
+###################################### this needs to be written in SQL ##########################################        
+
+        result = db.engine.execute("SELECT * FROM users")
+        for row in result:
+            # Ensure password is correct
+            if row['user_name'] == inputusername:
+
+#                if not check_password_hash(row["user_hash"], inputpassword):
+#                    return apology("Invalid password")
+                
+                # Remember which user has logged in
+                session["user_id"] = row["user_id"]
+
+                # Redirect user to home page
+                return redirect("/")
+        # if no usernames match then tell the user
+        return apology("That username does not exist")       
+###################################### this needs to be written in SQL ##########################################    
+      
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+###### available pages once users have logged in ######
+
+@app.route("/", methods=['GET', 'POST'])
+@login_required
+def myrecipes():
+
+
+    userrecipesinput = "(SELECT recipe_id FROM users_recipes WHERE user_id=" + str(session.get("user_id")) + ")"
+    sqlinput = "SELECT recipes.recipe_name, cuisines.cuisine_name, chefs.chef_name, dish_types.dish_type_name, recipes.link_book, recipes.page_number, recipes.time, recipes.complexity FROM recipes INNER JOIN cuisines ON recipes.cuisine_id=cuisines.cuisine_id INNER JOIN chefs ON recipes.chef_id=chefs.chef_id INNER JOIN dish_types ON recipes.dish_type_id=dish_types.dish_type_id WHERE recipe_id IN " + userrecipesinput + " ORDER BY cuisine_name, recipe_name ASC"
+    recipesdata = db.engine.execute(sqlinput)
+
+    return render_template("myrecipes.html", datasheet=recipesdata)
+
+
+@app.route("/addrecipes", methods=['GET', 'POST'])
+@login_required
+def addrecipes():
+
+    if request.method =="POST":
+        recipe = request.form.get("recipe").title()
+        cuisine = request.form.get("cuisine").title()
+        dish_type = request.form.get("dishtype").title()
+        chef = request.form.get("chef", None).title()
+        link = request.form.get("link", None)
+        page = request.form.get("page", None)
+        time = request.form.get("time", None)
+        complexity = request.form.get("complexity", None).title()
+
+        # making sure that data being inputted to database is defined
+        for item in db.engine.execute("SELECT recipe_name FROM recipes"):
+            if recipe.lower() == item[0].lower():
+                return render_template("apology.html", message="That recipe already exists!")
+                    
+                    
+        # checks if cuisine exists in 'cuisines' table and INSERTS it if not
+        cuisineid = insertifnot("cuisine", cuisine)
+        dish_typeid = insertifnot("dish_type", dish_type)
+        if chef == None:
+            chefid = None   
+        else:
+            chefid = insertifnot("chef", chef)
+                       
+
+        sqlsession.commit() 
+        # insert data from HTML form
+        sqlsession.execute(insert(Table('recipes', metadata, autoload=True)).values({
+            "recipe_name": recipe,
+            "cuisine_id": cuisineid,
+            "dish_type_id" : dish_typeid,
+            "chef_id" : chefid,
+            "link_book" : link,
+            "page_number" : page,
+            "time" : time,
+            "complexity" : complexity
+            }))
+        
+        # Find out the recipe_id
+        lastid = sqlsession.execute("SELECT LAST_INSERT_ID() AS recipe_id")
+        for row in lastid:
+            recipeid = row['recipe_id']
+
+        sqlsession.execute(insert(Table('users_recipes', metadata, autoload=True)).values({
+            "user_id": session.get("user_id"),
+            "recipe_id": recipeid
+            }))
+
+
+        # commit changes to the database
+        sqlsession.commit() 
+
+
+ 
+        return render_template("addrecipes.html")
+    else:
+        return render_template("addrecipes.html")
+
+
+@app.route("/allrecipes", methods=['GET', 'POST'])
+@login_required
+def allrecipes():
+
+    if request.method =="POST":
+
+        # receives an array of data values = ingredient_id from the matching checkboxes
+        result = request.form.getlist("recipe")
+
+        for recipeid in result:
+            # insert data from HTML form
+            sqlsession.execute(insert(Table('users_recipes', metadata, autoload=True)).values({
+                "user_id": session.get("user_id"),
+                "recipe_id": int(recipeid)
+                }))
+
+        # commit changes to the database
+        sqlsession.commit() 
+
+        return redirect("/")
+
+    else:
+
+        recipesdata = db.engine.execute("SELECT recipes.recipe_id, recipes.recipe_name, cuisines.cuisine_name, chefs.chef_name, dish_types.dish_type_name, recipes.link_book, recipes.page_number, recipes.time, recipes.complexity FROM recipes INNER JOIN cuisines ON recipes.cuisine_id=cuisines.cuisine_id INNER JOIN chefs ON recipes.chef_id=chefs.chef_id INNER JOIN dish_types ON recipes.dish_type_id=dish_types.dish_type_id ORDER BY cuisine_name, recipe_name ASC")
+        return render_template("allrecipes.html", datasheet=recipesdata)
+
+
+@app.route("/mypantry", methods=['GET', 'POST'])
+@login_required
+def mypantry():
+    useringredientsinput = "(SELECT ingredient_id FROM users_ingredients WHERE user_id=" + str(session.get("user_id")) + ")"
+    sqlinput = "SELECT ingredient_name, ingredient_type FROM ingredients WHERE ingredient_id IN " + useringredientsinput + " ORDER BY ingredient_type, ingredient_name ASC"
+    ingredientsdata = db.engine.execute(sqlinput)
+    return render_template("mypantry.html", datasheet=ingredientsdata)
+
+
+@app.route("/addingredients", methods=['GET', 'POST'])
+@login_required
+def addingredients():
+
+    if request.method =="POST":
+        # receives food information from HTML form and capitalises it
+        food_item = request.form.get("fooditem").title()
+        food_type = request.form.get("foodtype").title()
+        
+    
+        for item in db.engine.execute("SELECT ingredient_name FROM ingredients"):
+            if food_item.lower() == item[0].lower():
+                return apology("That ingredient already exists!")
+
+        # insert data from HTML form
+        sqlsession.execute(insert(Table('ingredients', metadata, autoload=True)).values({
+            "ingredient_name": food_item,
+            "ingredient_type": food_type
+            }))
+
+        # commit changes to the database
+        sqlsession.commit() 
+
+        return render_template("addingredients.html")
+    else:
+        return render_template("addingredients.html")
+
+
+
+@app.route("/allingredients", methods=['GET', 'POST'])
+@login_required
+def allingredients():
+
+    if request.method =="POST":
+
+        # receives an array of data values = ingredient_id from the matching checkboxes
+        result = request.form.getlist("ingredient")
+
+        for ingredientid in result:
+            # insert data from HTML form
+            sqlsession.execute(insert(Table('users_ingredients', metadata, autoload=True)).values({
+                "user_id": session.get("user_id"),
+                "ingredient_id": int(ingredientid)
+                }))
+
+        # commit changes to the database
+        sqlsession.commit() 
+
+        return redirect("/mypantry")
+
+    else:
+
+        fooddata = db.engine.execute("SELECT * FROM ingredients ORDER BY ingredient_type, ingredient_name ASC")
+        return render_template("allingredients.html", datasheet=fooddata)  
+
+
+
+@app.route("/apology")
+def apology(errormessage):
+
+    return render_template("apology.html", message=errormessage)
+
+if __name__ == "__main__":
+    app.run()
---
+++ initialise/create_database.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://updatepantry:K269210dL1ghtn1ng@localhost/thepantry'
+db = SQLAlchemy(app)
+
+class chefs(db.Model):
+    __tablename__ = 'chefs'
+    chef_id = db.Column(db.Integer, primary_key=True)
+    chef_name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<chefs %r>' % self.chef_name
+
+class cuisines(db.Model):
+    __tablename__ = 'cuisines'
+    cuisine_id = db.Column(db.Integer, primary_key=True)
+    cuisine_name = db.Column(db.String(40), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<cuisines %r>' % self.cuisine_name
+
+class dish_types(db.Model):
+    __tablename__ = 'dish_types'
+    dish_type_id = db.Column(db.Integer, primary_key=True)
+    dish_type_name = db.Column(db.String(40), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<dish_types %r>' % self.dish_type_name
+
+class ingredients(db.Model):
+    __tablename__ = 'ingredients'
+    ingredient_id = db.Column(db.Integer, primary_key=True)
+    ingredient_name = db.Column(db.String(40), unique=True, nullable=False)
+    ingredient_type = db.Column(db.String(40), nullable=False)
+    ingredient_approved = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return '<ingredients %r>' % (self.ingredient_name)
+
+class recipes(db.Model):
+    __tablename__ = 'recipes'
+    recipe_id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(60), unique=True, nullable=False)
+    cuisine_id = db.Column(db.Integer, db.ForeignKey("cuisines.cuisine_id"), nullable=False)
+    chef_id = db.Column(db.Integer, db.ForeignKey("chefs.chef_id"))
+    dish_type_id = db.Column(db.Integer, db.ForeignKey("dish_types.dish_type_id"), nullable=False)
+    link_book = db.Column(db.String(200))
+    page_number = db.Column(db.Integer)
+    time = db.Column(db.Integer)
+    complexity = db.Column(db.String(20))
+    recipe_approved = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return '<recipes %r>' % self.recipe_name
+
+class recipes_ingredients(db.Model):
+    __tablename__ = 'recipes_ingredients'
+    recipe_ingredient_id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.recipe_id"), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey("ingredients.ingredient_id"), nullable=False)
+
+    def __repr__(self):
+        return '<recipes_ingredients %r>' % self.recipe_ingredient_id
+
+class users(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    user_hash = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<users %r>' % self.users_name
+
+class users_recipes(db.Model):
+    __tablename__ = 'users_recipes'
+    user_recipe_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.recipe_id"), nullable=False)
+
+    def __repr__(self):
+        return '<users_recipes %r>' % self.user_recipe_id
+
+class users_ingredients(db.Model):
+    __tablename__ = 'users_ingredients'
+    user_ingredient_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey("ingredients.ingredient_id"), nullable=False)
+    ingredient_amount = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<users_ingredients %r>' % self.user_ingredient_id
